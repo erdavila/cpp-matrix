43f1fec4ab6304bb3adb1cfc17976e1544098d80
Eduardo R. D'Avila <erdavila@gmail.com>
2016-01-01 03:48:31 -0200

    Do basic implementation of storage

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,92 @@
 #include "matrix.hpp"
+#include "storage.hpp"
 #include <cassert>
 #include <type_traits>
+
+
+#define assert_throws(EXPR, EXCEPTION) \
+	try { \
+		EXPR; \
+		assert(false); \
+	} catch(EXCEPTION&) { \
+		assert(true); \
+	}
+
+
+namespace storage {
+	template <typename T>
+	using verified_storage = matrix::storage<T, true>;
+
+	void testRegularFlow()
+	try {
+		verified_storage<int> s;
+		s.construct_value(7);
+		assert(s.value_reference() == 7);
+		s.value_reference() = 3;
+		assert(s.value_reference() == 3);
+		s.destruct_value();
+	} catch(matrix::storage_verifier::exception&) {
+		assert(false);
+	}
+
+	void testUseValueNotConstructed()
+	try {
+		{
+			verified_storage<int> s;
+			assert_throws(s.value_reference(), matrix::storage_verifier::exception);
+		}
+
+		{
+			verified_storage<int> s;
+			s.construct_value();
+			s.destruct_value();
+			assert_throws(s.value_reference(), matrix::storage_verifier::exception);
+		}
+	} catch(matrix::storage_verifier::exception&) {
+		assert(false);
+	}
+
+	void testDoubleConstruction()
+	try {
+		verified_storage<int> s;
+		s.construct_value();
+		assert_throws(s.construct_value(), matrix::storage_verifier::exception);
+		s.destruct_value();
+	} catch(matrix::storage_verifier::exception&) {
+		assert(false);
+	}
+
+	void testDoubleDestruction()
+	try {
+		{
+			verified_storage<int> s;
+			assert_throws(s.destruct_value(), matrix::storage_verifier::exception);
+		}
+
+		{
+			verified_storage<int> s;
+			s.construct_value();
+			s.destruct_value();
+			assert_throws(s.destruct_value(), matrix::storage_verifier::exception);
+		}
+	} catch(matrix::storage_verifier::exception&) {
+		assert(false);
+	}
+
+	void test() {
+		testRegularFlow();
+		testUseValueNotConstructed();
+		testDoubleConstruction();
+		testDoubleDestruction();
+	}
+} /* namespace storage */
+
+
+namespace safely_constructed_array {
+	void test() {
+
+	}
+} /* namespace safely_constructed_array */
 
 
 namespace smatrix {
@@ -45,6 +131,8 @@ namespace common {
 
 
 int main() {
+	storage::test();
+	safely_constructed_array::test();
 	smatrix::test();
 	dmatrix::test();
 	common::test();
---
+++ storage.hpp
@@ -0,0 +1,108 @@
+#ifndef STORAGE_HPP_
+#define STORAGE_HPP_
+
+#include <new>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+
+namespace matrix {
+
+
+class storage_verifier {
+public:
+	class exception : std::logic_error {
+		using std::logic_error::logic_error;
+	};
+
+protected:
+	void set_constructed(bool constructed) noexcept {
+		this->constructed = constructed;
+	}
+
+	void verify_constructed(bool expected) const {
+		if(constructed != expected) {
+			const char* msg = expected
+			                ? "The object was expected to be constructed"
+			                : "The object was expected to be not constructed";
+            throw exception(msg);
+		}
+	}
+
+private:
+	bool constructed = false;
+};
+
+
+class null_storage_verifier {
+protected:
+	void set_constructed(bool) noexcept {}
+	void verify_constructed(bool) const {}
+};
+
+
+template <typename T, bool Verified = false>
+class storage : private std::conditional<Verified, storage_verifier, null_storage_verifier>::type {
+public:
+	using value_type = T;
+
+	storage() = default;
+
+	storage(const storage&);
+
+	storage(storage&&);
+
+	~storage() {
+		verify_constructed(false);
+	}
+
+	storage& operator=(const storage&) &;
+
+	storage& operator=(storage&&) &;
+
+	template <typename... Args>
+	void construct_value(Args&&... args) {
+		verify_constructed(false);
+		new(&store) T(std::forward<Args>(args)...);
+		set_constructed(true);
+	}
+
+	void destruct_value() {
+		verify_constructed(true);
+		value_reference().~T();
+		set_constructed(false);
+	}
+
+	T& value_reference() {
+		return *value_pointer();
+	}
+
+	const T& value_reference() const;
+
+private:
+	using verifier_type = typename std::conditional<Verified, storage_verifier, null_storage_verifier>::type;
+	using verifier_type::verify_constructed;
+	using verifier_type::set_constructed;
+
+	using store_type = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
+	store_type store;
+
+	T* value_pointer() {
+		verify_constructed(true);
+		void* p = &store;
+		return static_cast<T*>(p);
+	}
+
+	const T* value_pointer() const;
+};
+
+
+static_assert(sizeof(storage<int, false>) == sizeof(int), "");
+static_assert(sizeof(storage<int, true> ) >  sizeof(int), "");
+
+
+} /* namespace matrix */
+
+
+#endif /* STORAGE_HPP_ */
